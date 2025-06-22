2d2fe9def3bd3a2374d946ea2e6e3b2341116b0a
Abhijith-PJ <pjfaruachu@gmail.com>
2025-06-23 02:19:42 +0530

    Add expense list screen with API integration

--- frontend/expense-frontend/app/_layout.tsx
+++ frontend/expense-frontend/app/_layout.tsx
@@ -1,13 +1,11 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
+import { PaperProvider } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -18,12 +16,10 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+   <SafeAreaProvider>
+      <PaperProvider>
+        <Stack />
+      </PaperProvider>
+    </SafeAreaProvider>
   );
 }
